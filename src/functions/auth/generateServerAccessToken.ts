3a0b7edf8123ea26264d0f65ea2d1ba01631af16
Rodwell Leo <leorodwel86@gmail.com>
2025-07-11 20:53:10 +0300

    refactor: update server access token generation and socket connection
    - Refactored generateServerAccessToken to use environment variables for backend URL.
    - Improved error handling in generateServerAccessToken function.
    - Updated socket connection to utilize environment variable for server URL instead of hardcoded value.

--- src/functions/auth/generateServerAccessToken.ts
+++ src/functions/auth/generateServerAccessToken.ts
@@ -1,26 +1,28 @@
-
-import axios from "axios"
+import axios from "axios";
 
 const generateServerAccessToken = async () => {
+  try {
+    const username = import.meta.env.VITE_AUTH_USERNAME!;
+    const password = import.meta.env.VITE_AUTH_PASSWORD!;
+    const base_url = import.meta.env.VITE_BACKEND_PROD_SERVER_URL!;
+    const res = await axios.post(
+      `${base_url}/auth/login`,
+      {
+        username,
+        password,
+      },
+      {
+        headers: {
+          Content: "application/json",
+        },
+      }
+    );
 
-    try {
-        const username = import.meta.env.VITE_AUTH_USERNAME!
-        const password = import.meta.env.VITE_AUTH_PASSWORD!
+    const { accessToken } = res.data;
+    return { accessToken };
+  } catch (e) {
+    return null;
+  }
+};
 
-        const res = await axios.post('http://localhost:3000/auth/login', {
-            username,
-            password
-        }, {
-            headers: {
-                "Content": "application/json"
-            }
-        })
-
-        const { accessToken } = res.data
-        return { accessToken }
-    } catch (e) {
-        return null
-    }
-}
-
-export default generateServerAccessToken
+export default generateServerAccessToken;
--- src/utils/socket.io/index.ts
+++ src/utils/socket.io/index.ts
@@ -1,6 +1,5 @@
-
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = "https://avapay-backend.fly.dev";
+const SOCKET_SERVER_URL = import.meta.env.VITE_BACKEND_PROD_SERVER_URL!;
 
 export const socket = io(SOCKET_SERVER_URL);
